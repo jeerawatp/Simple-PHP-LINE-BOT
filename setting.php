c4455fec44f53892177962b080280dc661cdbb1f
jeerawat permsup <p.jeerawat.th@live.com>
2018-05-17 14:46:54 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,12 +2,12 @@
 
 class Setting {
 	public function getChannelAccessToken(){
-		$channelAccessToken = "qinKiQgfB0eNNqp15Pb70ldUHjlOxICsg6TMzcl70mCPIyBe9nNOngpTRUqxpnhgxW0NmtpGH2xP/UMkx4JNUkdzDhcQaOqHapy8yfAqCElBYHhZvVqM9ri9hXn2AbkM4XuiQoJp0pCzK9rF9HY9SwdB04t89/1O/w1cDnyilFU=
-";
+		$channelAccessToken = 'qinKiQgfB0eNNqp15Pb70ldUHjlOxICsg6TMzcl70mCPIyBe9nNOngpTRUqxpnhgxW0NmtpGH2xP/UMkx4JNUkdzDhcQaOqHapy8yfAqCElBYHhZvVqM9ri9hXn2AbkM4XuiQoJp0pCzK9rF9HY9SwdB04t89/1O/w1cDnyilFU=
+';
 		return $channelAccessToken;
 	}
 	public function getChannelSecret(){
-		$channelSecret = "4d93979b89f308b70acdadd9ec944f73";
+		$channelSecret = '4d93979b89f308b70acdadd9ec944f73';
 		return $channelSecret;
 	}
 	public function getApiReply(){
